dbe8e7db485456d37de53014c0b7926027420b10
Alejandro <josealejandroibarra@hotmail.com>
2019-09-03 22:00:04 -0600

    Ahora con todas las causas, soluciones, y referencias.

--- db.pl
+++ db.pl
@@ -18,6 +18,10 @@ respuestas(despedida, [
         ['Buen dia, amigo!']
         ]).
 
+respuestas(especial, [
+        ['Lo siento, contigo no hablare. Que tengas un mal, mal dia.']
+        ]).
+
 respuestas(saludo, [
         ['Hola!'],
         ['Hola, mucho gusto. :)'],
@@ -126,6 +130,145 @@ respuestas(respuestas_aleatorias, [
         ['Estoy de acuerdo con lo que dices.']
         ]).
 
+% BASE DE DATOS DE RESPUESTAS
+% LAS SOLUCIONES A PROBLEMAS SE NUMERAN CON S + NUMERO DE SOLUCION DE LA TABLA CSR
+
+respuestas(impresora, [
+        ['Conecte la impresora a la electricidad.'], %S1
+        ['Proceda a encender la impresora.'], %S2
+        ['Reinicie la impresora.'], %S3
+        ['Instale los drivers de la impresora en su computadora.'], %S4
+        ['Conecte su computadora a la red WiFi de la impresora.'], %S5
+        ['Reemplaze la tinta en su impresora.'], %S6
+        ['Cargue el papel en el portapapeles de su impresora.'], %S7
+        ['Siga el procedimiento de alineamiento de cabezales de su impresora.'], %S8
+        ['Limpie su impresora con mucho cuidado.'], %S9
+        ['Compre una nueva impresora.'] %S10
+        ]).
+
+respuestas(computadora, [
+        ['Conecte la computadora a la electricidad.'], %S1
+        ['Proceda a encender la computadora presionando el botón de Touch ID.'], %S2
+        ['Reinicie la computadora presionando el botón de Touch ID dos veces.'], %S3
+        ['Actualice el sistema operativo en Preferencias del Sistema.'], %S4
+        ['Conecte su computadora a una red WiFi.'], %S5
+        ['Conecte su cuenta de iCloud en Preferencias del Sistema -> iCloud.'], %S6
+        ['Libere espacio de su computadora desde el menú Apple -> Almacenamiento.'], %S7
+        ['Súbale el volumen a su computadora, tocando el icono de volumen en la Touch Bar.'], %S8
+        ['Limpie su computadora con mucho cuidado.'], %S9
+        ['Compre una nueva computadora.'] %S10
+        ]).
+
+respuestas(parlante, [
+        ['Conecte su parlante a la electricidad.'], %S1
+        ['Conecte el servicio de streaming de su preferencia en la app Controller.'], %S2
+        ['Subale el volumen al audio desde su dispositivo controlador o desde el parlante.'], %S3
+        ['Toque el icono de Play en su dispositivo controlador o en el parlante.'], %S4
+        ['Inicie el proceso de configuracion de red para el parlante.'], %S5
+        ['Conectese a la red WiFi donde esta su palante.'], %S6
+        ['Utilice un dispositivo soportado por la app Controller.'], %S7
+        ['Active Alexa desde la app Controller.'], %S8
+        ['Limpie su parlante con mucho cuidado.'], %S9
+        ['Compre un nuevo parlante.'] %S10
+        ]).
+
+respuestas(audifonos, [
+        ['Cargue sus audifonos con el cable USB-C incluido.'], %S1
+        ['Conecte sus audionos al dispositivo por medio de Bluetooth.'], %S2
+        ['Coloque bien sus audifonos en su cabeza.'], %S3
+        ['Cambie las cubiertas de los oidos para sus audifonos por unas nuevas.'], %S4
+        ['Actualice el software de sus audifonos desde la app movil.'], %S5
+        ['Reinicie el software de sus audifonos.'], %S6
+        ['Subale el volumen a sus audifonos desde el dispositivo que controla el audio.'], %S7
+        ['Toque el boton de Play en el dispositivo que controla el audio.'], %S8
+        ['Limpie sus audifonos con mucho cuidado.'], %S9
+        ['Compre unos nuevos audifonos.'] %S10
+        ]).
+
+respuestas(pareja, [
+        ['Demuestre interes genuino por los problemas de su pareja. Puede comenzar con una conversacion casual.'], %S1
+        ['Lo mas recomendable en estos casos es terminar la relacion. Una persona toxica solo trae implicaciones negativas para su vida.'], %S2
+        ['Una sugerencia es generar empatia. Ponerse en sus zapatos para entender sus sentimientos en este momento.'], %S3
+        ['Hable con el/ella para genera espacios de dialogo, donde ambos puedan discutir acerca del estado de su relacion.'], %S4
+        ['Intente buscar intereses comunes, aunque sean superficiales. Una serie, pelicula, viajar...'], %S5
+        ['Es importante mostrar que usted apoya a su pareja. Todos buscamos un apoyo en las personas que nos rodean, en especial de las que mas queremos.'], %S6
+        ['Comprenda el pensamiento de su pareja. Reflexione acerca de las razones que lo pueden llevar a pensar de esa manera.'], %S7
+        ['Limpie a su pareja con mucho cuidado.'], %S8
+        ['En estos casos, lo mejor es buscarse uno/a nuevo/a. A veces es mejor dejarlo morir. Que queda?'], %S9
+        ['Hay que alinearle la cabeza con un martillo astral de otra dimension.'] %S10
+        ]).
+
+% BASE DE DATOS DE PREGUNTAS.
+% LAS CAUSAS DE PROBLEMAS SE NUMERAN CON C + NÚMERO DE CAUSA DE LA TABLA CSR.
+
+questions_db(impresora, [
+        ['Esta conectada a la corriente?'], %C1
+        ['Esta encendida?'], %C2
+        ['Hay algun movimiento o respuesta?'], %C3
+        ['Tiene los drivers instalados?'], %C4
+        ['Su computadora esta en la misma red WiFi de la impresora?'], %C5
+        ['Tiene tinta?'], %C6
+        ['Tiene papel?'], %C7
+        ['Los cabezales estan alineados?'], %C8
+        ['Esta sucia?'], %C9
+        ['Esta vieja?'] %C10
+        ]).
+
+questions_db(computadora, [
+        ['Esta cargada?'], %C1
+        ['Esta encendida?'], %C2
+        ['Hay alguna respuesta en la pantalla o Touch Bar?'], %C3
+        ['El sistema operativo esta actualizado?'], %C4
+        ['Esta conectada a WiFi?'], %C5
+        ['Esta conectada a iCloud?'] %C6
+        ['El disco de almacenamiento esta lleno?'], %C7
+        ['El volumen esta alto?'], %C8
+        ['Esta sucia?'], %C9
+        ['Esta vieja?'] %C10
+        ]).
+
+questions_db(parlante, [
+        ['Esta conectado a la corriente?'], %C1
+        ['Los servicios de streaming estan activados?'] %C2
+        ['El volumen esta alto?'], %C3
+        ['Esta pausada la reproduccion?'], %C4
+        ['El parlante esta conectado a la red WiFi?'], %C5
+        ['El dispositivo controlador esta en la misma red WiFi del parlante?'], %C6
+        ['El dispositivo controlador esta soportado por la aplicacion?'] %C7
+        ['Alexa esta activada?'], %C8
+        ['Esta sucio?'], %C9
+        ['Esta viejo?'] %C10
+        ]).
+
+questions_db(audifonos, [
+        ['Estan cargados?'], %C1
+        ['Estan conectados al dispositivo con el audio?'] %C2
+        ['Estan bien colocados?'], %C3
+        ['Las cubiertas de los oidos estan en buen estado?'], %C4
+        ['El software esta actualizado?'], %C5
+        ['Se conecta con estabilidad al dispositivo con el audio?'], %C6
+        ['El volumen esta alto?'] %C7
+        ['El audio esta pausado?'], %C8
+        ['Estan sucios?'], %C9
+        ['Estan viejos?'] %C10
+        ]).
+
+questions_db(pareja, [
+        ['Lo/la nota triste?'], %C1
+        ['Ha notado algun comportamiento de depresion en el/ella?'], %C1
+        ['Considera que es alguien toxico para usted?'] %C2
+        ['Se siente drenado emocionalmente cuando lo/la ve?'] %C2
+        ['Lo/la fuerza a hacer cosas que usted no quiere?'] %C2
+        ['Considera que se siente emocionalmente desconectado de usted?'], %C3
+        ['Considera que el/ella tiene verguenza de sí mismo/misma?'], %C4
+        ['Considera que estan en la misma red/tiene la misma perspectiva sobre el mundo que usted?'], %C5
+        ['Le parece a usted que el/ella tiene miedo de alguna situacion?'], %C6
+        ['Ha manifestado alguna duda acerca de mantener la relacion?'] %C7
+        ['Esta sucio?'], %C8
+        ['Esta viejo?'], %C9
+        ['Tiene la cabeza desalineada?'] %C10
+        ]).
+
 questions_db(feedback, [
         ['Okay. Did you find any of the talks interesting?'],
         ['Hmm. Do you think the open day has been well organised?'],
@@ -157,11 +300,7 @@ gracias([
 
 dispositivos([impresora,
         computadora,
-        mac,
         parlante,
-        tablet,
-        calculadora,
-        wifi,
-        telefono,
-        conmigo
+        audifonos,
+        pareja
         ]).
