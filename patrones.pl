c2fe77095ba120edc69215a7b55628627cf1f8b0
Esteban Alvarado <estalvgs1999@gmail.com>
2019-09-07 22:37:58 -0600

    Cambios finales en el analizador de patrones

--- patrones.pl
+++ patrones.pl
@@ -72,6 +72,7 @@ patronReferencia([tendras, referencias, X|_],X):-!.
 patronReferencia([puedo, conseguir, informacion, X|_],X):-!.
 patronReferencia([tienes, informacion, acerca, de, X|_],X):-!.
 patronReferencia([necesito, conocer, X|_],X):-!.
+patronReferencia([necesito, informacion, X|_],X):-!.
 patronReferencia([tienes, alguna, referencia, X|_],X):-!.
 patronReferencia([referencia, X|_],X):-!.
 patronReferencia([referencias, X|_],X):-!.
@@ -118,19 +119,21 @@ patronProblema([_|T], X):-
 %patronProbRef/4
 
 % Problema-Referencia de impresora
-patronProbRef(impresora,[no, imprime, X|_],X, [2,6,7]):- !.
-patronProbRef(impresora,[no, enciende, X|_],X, [1,2]):- !.
-patronProbRef(impresora,[no, salen, las, impresiones, X|_],X, [6]):- !.
-patronProbRef(impresora,[la, impresion, esta, borrosa, X|_],X,[6,8,9]):- !.
-patronProbRef(impresora,[la, impresion, sale, sucia, X|_],X, [8,9]):- !.
+patronProbRef(impresora,[no, imprime, X|_],X, [9,5,4]):- !.
+patronProbRef(impresora,[no, enciende, X|_],X, [10,9]):- !.
+patronProbRef(impresora,[no, salen, las, impresiones, X|_],X, [5]):- !.
+patronProbRef(impresora,[la, impresion, esta, borrosa, X|_],X,[5,3,2]):- !.
+patronProbRef(impresora,[la, impresion, sale, sucia, X|_],X, [3,2]):- !.
 
 % Problema-Referencia de computadora
-
+patronProbRef(computadora,[no, trabaja, X|_],X, [10,9,4]):- !.
 
 % Problema-Referencia de parlante
-
+patronProbRef(parlante,[no, suena, X|_],X, [10,9,8,7]):- !.
 
 % Problema-Referencia de audifonos
+patronProbRef(audifonos,[no, suenan, X|_],X, [9,4,3]):- !.
+patronProbRef(audifonos,[no, se, escuchan, X|_],X, [9,4,3]):- !.
 
 patronProbRef(D,[_|T],X,N):-
   patronProbRef(D,T,X,N).
@@ -140,54 +143,54 @@ patronProbRef(D,[_|T],X,N):-
 % Busca patrones en las causas de un problemas
 
 % Causas impresora
-patronCausa(impresora,[no, esta, conectada, X |_], X, NS):- !, NS is 1.
-patronCausa(impresora,[no, esta, encendida, X |_], X, NS):- !, NS is 2.
-patronCausa(impresora,[no, enciende, X |_], X, NS):- !, NS is 2.
-patronCausa(impresora,[no, responde, X |_], X, NS):- !, NS is 3.
-patronCausa(impresora,[no, tiene, drivers, X |_], X, NS):- !, NS is 4.
-patronCausa(impresora,[no, esta, en, la, misma, red, wifi, X |_], X, NS):- !, NS is 5.
-patronCausa(impresora,[no, tiene, tinta, X |_], X, NS):- !, NS is 6.
-patronCausa(impresora,[no, tiene, papel, X |_], X, NS):- !, NS is 7.
-patronCausa(impresora,[cabezales, desalineados, X|_], X, NS):- !, NS is 8.
-patronCausa(impresora,[esta, sucia, X |_], X, NS):- !, NS is 9.
-patronCausa(impresora,[esta, vieja, X |_], X, NS):- !, NS is 10.
+patronCausa(impresora,[no, esta, conectada, X |_], X, NS):- !, NS is 10.
+patronCausa(impresora,[no, esta, encendida, X |_], X, NS):- !, NS is 9.
+patronCausa(impresora,[no, enciende, X |_], X, NS):- !, NS is 9.
+patronCausa(impresora,[no, responde, X |_], X, NS):- !, NS is 8.
+patronCausa(impresora,[no, tiene, drivers, X |_], X, NS):- !, NS is 7.
+patronCausa(impresora,[no, esta, en, la, misma, red, wifi, X |_], X, NS):- !, NS is 6.
+patronCausa(impresora,[no, tiene, tinta, X |_], X, NS):- !, NS is 5.
+patronCausa(impresora,[no, tiene, papel, X |_], X, NS):- !, NS is 4.
+patronCausa(impresora,[cabezales, desalineados, X|_], X, NS):- !, NS is 3.
+patronCausa(impresora,[esta, sucia, X |_], X, NS):- !, NS is 2.
+patronCausa(impresora,[esta, vieja, X |_], X, NS):- !, NS is 1.
 
 % Causas computadora
-patronCausa(computadora, [esta, descargada, X |_], X, NS):-! ,NS is 1.
-patronCausa(computadora, [no, enciende, X |_], X, NS):- !, NS is 2.
-patronCausa(computadora, [no, esta, encendida, X |_], X, NS):-!, NS is 2.
-patronCausa(computadora, [no, responde, X |_], X, NS):- !, NS is 3.
-patronCausa(computadora, [no, esta, actualizada, X |_], X, NS):-!, NS is 4.
-patronCausa(computadora, [no, esta, conectada, al, wifi, X |_], X, NS):-!, NS is 5.
-patronCausa(computadora, [esta, desconectada, de, sus, datos, X |_], X, NS):-!, NS is 6.
-patronCausa(computadora, [esta, llena, X |_], X, NS):-!, NS is 7.
-patronCausa(computadora, [volumen, bajo, X |_], X, NS):-!, NS is 8.
-patronCausa(computadora, [esta, sucia, X |_], X, NS):- !, NS is 9.
-patronCausa(computadora, [esta, vieja, X |_], X, NS):- !, NS is 10.
+patronCausa(computadora, [esta, descargada, X |_], X, NS):-! ,NS is 10.
+patronCausa(computadora, [no, enciende, X |_], X, NS):- !, NS is 9.
+patronCausa(computadora, [no, esta, encendida, X |_], X, NS):-!, NS is 9.
+patronCausa(computadora, [no, responde, X |_], X, NS):- !, NS is 8.
+patronCausa(computadora, [no, esta, actualizada, X |_], X, NS):-!, NS is 7.
+patronCausa(computadora, [no, esta, conectada, al, wifi, X |_], X, NS):-!, NS is 6.
+patronCausa(computadora, [esta, desconectada, de, sus, datos, X |_], X, NS):-!, NS is 5.
+patronCausa(computadora, [esta, llena, X |_], X, NS):-!, NS is 4.
+patronCausa(computadora, [volumen, bajo, X |_], X, NS):-!, NS is 3.
+patronCausa(computadora, [esta, sucia, X |_], X, NS):- !, NS is 2.
+patronCausa(computadora, [esta, vieja, X |_], X, NS):- !, NS is 1.
 
 % Causas Parlante
-patronCausa(parlante, [no, esta, conectado, X |_], X, NS):- !, NS is 1.
-patronCausa(parlante, [servicios, de, streaming, no, conectados, X |_], X, NS):-!, NS is 2.
-patronCausa(parlante, [volumen, bajo, X |_], X, NS):-!, NS is 3.
-patronCausa(parlante, [esta, pausado, X |_], X, NS):-!, NS is 4.
-patronCausa(parlante, [no, esta, conectado, a, la, red, X |_], X, NS):-!, NS is 5.
-patronCausa(parlante, [no, esta, en, la, misma, red, wifi, X |_], X, NS):-!, NS is 6.
-patronCausa(parlante, [el, sistema, no, esta, soportado, X |_], X, NS):- !, NS is 7.
-patronCausa(parlante, [alexa, desactivada, X |_], X, NS):- !, NS is 8.
-patronCausa(parlante, [esta, sucia, X |_], X, NS):- !, NS is 9.
-patronCausa(parlante, [esta, vieja, X |_], X, NS):- !, NS is 10.
+patronCausa(parlante, [no, esta, conectado, X |_], X, NS):- !, NS is 10.
+patronCausa(parlante, [servicios, de, streaming, no, conectados, X |_], X, NS):-!, NS is 9.
+patronCausa(parlante, [volumen, bajo, X |_], X, NS):-!, NS is 8.
+patronCausa(parlante, [esta, pausado, X |_], X, NS):-!, NS is 7.
+patronCausa(parlante, [no, esta, conectado, a, la, red, X |_], X, NS):-!, NS is 6.
+patronCausa(parlante, [no, esta, en, la, misma, red, wifi, X |_], X, NS):-!, NS is 5.
+patronCausa(parlante, [el, sistema, no, esta, soportado, X |_], X, NS):- !, NS is 4.
+patronCausa(parlante, [alexa, desactivada, X |_], X, NS):- !, NS is 3.
+patronCausa(parlante, [esta, sucia, X |_], X, NS):- !, NS is 2.
+patronCausa(parlante, [esta, vieja, X |_], X, NS):- !, NS is 1.
 
 % Causas audifonos
-patronCausa(audifonos, [no, estan, cargados, X |_], X, NS):- !, NS is 1.
-patronCausa(audifonos, [no, estan, conectados, X |_], X, NS):- !, NS is 2.
-patronCausa(audifonos, [estan, mal, colocados, X |_], X, NS):- !, NS is 3.
-patronCausa(audifonos, [las, cubiertas, estan, deterioradas, X |_], X, NS):- !, NS is 4.
-patronCausa(audifonos, [estan, desactualizados, X |_], X, NS):- !, NS is 5.
-patronCausa(audifonos, [no, se, conectan, X |_], X, NS):- !, NS is 6.
-patronCausa(audifonos, [volumen, bajo, X |_], X, NS):-!, NS is 7.
-patronCausa(audifonos, [pausados, X |_], X, NS):- !, NS is 8.
-patronCausa(audifonos, [estan, sucios, X |_], X, NS):- !, NS is 9.
-patronCausa(audifonos, [estan, viejos, X |_], X, NS):- !, NS is 10.
+patronCausa(audifonos, [no, estan, cargados, X |_], X, NS):- !, NS is 10.
+patronCausa(audifonos, [no, estan, conectados, X |_], X, NS):- !, NS is 9.
+patronCausa(audifonos, [estan, mal, colocados, X |_], X, NS):- !, NS is 8.
+patronCausa(audifonos, [las, cubiertas, estan, deterioradas, X |_], X, NS):- !, NS is 7.
+patronCausa(audifonos, [estan, desactualizados, X |_], X, NS):- !, NS is 6.
+patronCausa(audifonos, [no, se, conectan, X |_], X, NS):- !, NS is 5.
+patronCausa(audifonos, [volumen, bajo, X |_], X, NS):-!, NS is 4.
+patronCausa(audifonos, [pausados, X |_], X, NS):- !, NS is 3.
+patronCausa(audifonos, [estan, sucios, X |_], X, NS):- !, NS is 2.
+patronCausa(audifonos, [estan, viejos, X |_], X, NS):- !, NS is 1.
 
 % patronCausa( Dispositivo, Lista, Comparador, Número de Solución).
 patronCausa(D, [_|T], X, NS):-
